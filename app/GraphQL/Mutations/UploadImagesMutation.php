edea8f49316d46510fa478862a1130d1282bda28
saad ouchama <saadochama@gmail.com>
2024-03-24 01:19:15 +0000

    fix the issues of upload images

--- app/GraphQL/Mutations/UploadImagesMutation.php
+++ app/GraphQL/Mutations/UploadImagesMutation.php
@@ -4,40 +4,22 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Mutations;
 
-use GraphQL\Type\Definition\Type;
 use Illuminate\Support\Facades\Storage;
-use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
-
 
 class UploadImagesMutation
 {
-    protected $attributes = [
-        'name' => 'uploadImages',
-    ];
-
-    public function type(): Type
+    public function __invoke($rootValue, array $args)
     {
-        return Type::nonNull(Type::listOf(Type::string()));
-    }
-
-    public function args(): array
-    {
-        return [
-            'files' => ['name' => 'files', 'type' => Type::nonNull(Type::listOf(Type::upload()))],
-        ];
-    }
-
-    public function __invoke($rootValue, array $args, GraphQLContext $context)
-
-    {
-        $imageUrls = [];
+        $filePaths = [];
 
         foreach ($args['files'] as $file) {
-            // Store the uploaded image
-            $path = $file->store('products', 'public');
-            $imageUrls[] = Storage::url($path);
+            // Validate the file, if necessary
+
+            // Store the file and get the path
+            $filePath = $file->store('images', 'public');
+            $filePaths[] = Storage::disk('public')->url($filePath);
         }
 
-        return $imageUrls;
+        return $filePaths;
     }
 }

30affeba3a1123b92f58b6c52d97356ce8f580b3
saad ouchama <saadochama@gmail.com>
2024-03-25 21:20:35 +0000

    optimize images upload mutation

--- app/GraphQL/Mutations/UploadImagesMutation.php
+++ app/GraphQL/Mutations/UploadImagesMutation.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\GraphQL\Mutations;
 
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class UploadImagesMutation
 {
@@ -13,11 +14,22 @@ class UploadImagesMutation
         $filePaths = [];
 
         foreach ($args['files'] as $file) {
-            // Validate the file, if necessary
+            // Validate the file
+            $validator = Validator::make(['file' => $file], [
+                'file' => 'required|file|mimes:jpg,jpeg,png,webp|max:2048', // Example validation
+            ]);
+
+            if ($validator->fails()) {
+                throw new \Exception($validator->errors()->first());
+            }
 
             // Store the file and get the path
             $filePath = $file->store('images', 'public');
-            $filePaths[] = Storage::disk('public')->url($filePath);
+            if ($filePath) {
+                $filePaths[] = Storage::disk('public')->url($filePath);
+            } else {
+                throw new \Exception("Failed to store the file.");
+            }
         }
 
         return $filePaths;

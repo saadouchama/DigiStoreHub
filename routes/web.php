fc1f7b5c00b14c5e005662b01921fd6134831d87
saad ouchama <saadochama@gmail.com>
2024-03-31 01:28:39 +0000

    add a route to download product file if the current user has permission

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Response;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +23,27 @@ Route::get('/', function () {
 Route::get('/checkout', function () {
     return view('checkout');
 });
+Route::get('/product-file/{productId}', function ($productId) {
+    $user = Auth::guard('sanctum')->user();
+
+    if (!$user) {
+        abort(403, 'Not Authorized');
+    }
+
+    $product = Product::find($productId);
+
+    // Check if the product exists and if the user has permission
+    if ($product && $user->hasPurchased($product)) {
+        $filePath = $product->file_path;
+
+        if (Storage::exists($filePath)) {
+            return Response::file(Storage::path($filePath));
+        } else {
+            abort(404, 'File not found');
+        }
+    } else {
+        abort(403, 'Access denied');
+    }
+})->middleware(['auth:sanctum']);
 
 require __DIR__.'/auth.php';

521d75c586b1f578ddbd91c1731c203b670a7bbd
Saad Ouchama <saadochama@gmail.com>
2024-03-31 06:08:59 +0000

    fix review model to use mongoDB Model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,18 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 
 class Review extends Model
 {
-    use HasFactory;
 
     protected $connection = 'mongodb';
     // Specify your collection if not default 'reviews'
     protected $collection = 'reviews';
     // Fillable, hidden, casts attributes as per your requirements
-    protected $fillable = ['product_id', 'user_id', 'comment', 'rating', 'date', 'updated_at'];
+    protected $fillable = ['product_id', 'user_id', 'comment', 'rating', 'created_at', 'updated_at'];
 
     public function product()
     {
